e9408fc4b2ad01b59ba58fd78852d729bb6d31e3
abhinav <contact.abhinavanshul@gmail.com>
2023-04-05 18:37:57 +0530

    fix : tsup bundle error for browser

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import React from "react"
 import { createRoot } from "react-dom/client"
+import App from "./App"
 
 const root = createRoot(document.getElementById("root")!)
 root.render(
   <React.StrictMode>
-    <h1>hello</h1>
+    <App />
   </React.StrictMode>
 )
--- tsup.config.ts
+++ tsup.config.ts
@@ -2,13 +2,16 @@ import { defineConfig } from "tsup"
 
 export default defineConfig({
   minify: true,
-  target: "es2018",
-  external: ["react"],
+  target: "es2015",
+  // external: ["react"],
   sourcemap: true,
   dts: true,
-  format: ["esm", "cjs"],
+  format: ["esm", "cjs", "iife"],
   injectStyle: true,
   esbuildOptions(options) {
+    options.define = {
+      "process.env.NODE_ENV": JSON.stringify("production"),
+    }
     options.banner = {
       js: '"use client"',
     }
